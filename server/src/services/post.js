74fedf97e23d0533051a17780d7e31419f4f346c
hyae4924 <hyae4924@gmail.com>
2022-12-08 03:05:46 +0900

    [FIX] Fix create,get post API

--- server/src/services/post.js
+++ server/src/services/post.js
@@ -1,25 +1,37 @@
 import data from "../data/index.js";
 
-// get의 쿼리같은경우는 필드명만 살짝 수정해주면 되어서 그냥 변수를 한꺼번에 넣는 방식으로
-//쿼리 설정을 함
-export async function getPost(postType, kind1rm, userId, number, limit) {
-  return await data.postRepo.getPost(postType, kind1rm, userId, number, limit);
+// get의 쿼리같은경우는 필드명만 살짝 수정해주면 되어서
+// 그냥 변수를 한꺼번에 넣는 방식으로 쿼리 설정을 함
+export async function getPost(params, query) {
+  const { postType } = params;
+  const { kind1rm, userId, number, limit } = query;
+  const posts = await data.postRepo.getPost(
+    postType,
+    kind1rm,
+    userId,
+    number,
+    limit
+  );
+
+  return posts.map(post => {
+    const location = JSON.parse(post.content);
+    return { ...post, content: location };
+  });
 }
 
-//
 export async function createPost(params, body, files) {
   const { userId, text } = body;
   const user_id = (await data.userReop.findByUserId(userId)).id;
-
   const { postType } = params;
-  const location = files.map(content => content.location);
+  const location = files.map(file => file.location);
   const content = await JSON.stringify(location);
+  let post;
   if (postType === "ohwunwan" || postType === "feedback") {
-    return data.postRepo.createPost(postType, user_id, text, content);
+    post = await data.postRepo.createPost(postType, user_id, text, content);
   }
   if (postType === "1rm") {
     const { kg, kind1rm } = body;
-    return data.postRepo.createPost1rm(
+    post = await data.postRepo.createPost1rm(
       postType,
       user_id,
       text,
@@ -28,4 +40,7 @@ export async function createPost(params, body, files) {
       kind1rm
     );
   }
+  return post.map(item => {
+    return { ...item, content: JSON.parse(item.content) };
+  });
 }
