2a51d56087200d2ac4d8e0a78732df871d7692d1
hackjoong <nuclearjoong@gmail.com>
2022-12-14 13:44:59 +0900

    [FEAT] Add ohwunwan page axios

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 // components
 import Article from "./Article";
@@ -8,36 +10,54 @@ import userImage from "../assets/icons/circle-user-solid.svg";
 import articleImage from "../assets/images/workout1.jpeg";
 
 function Main() {
-  const articleLists = {
-    articles: [
-      {
-        userId: "sonny",
-        userName: "손흥민",
-        userImage: {userImage},
-        articleImage: {articleImage},
-        text: "오늘의 운동 완료!",
-        comments: []
-      },
-      {
-        userId: "hee",
-        userName: "황희찬",
-        userImage: {userImage},
-        articleImage: {articleImage},
-        text: "나도 오운완!",
-        comments: []
-      }
-    ]
+  const [articleLists, setArticleLists] = useState([])
+
+  // const articleLists = {
+  //   articles: [
+  //     {
+  //       userId: "sonny",
+  //       userName: "손흥민",
+  //       userImage: {userImage},
+  //       articleImage: {articleImage},
+  //       text: "오늘의 운동 완료!",
+  //       comments: []
+  //     },
+  //     {
+  //       userId: "hee",
+  //       userName: "황희찬",
+  //       userImage: {userImage},
+  //       articleImage: {articleImage},
+  //       text: "나도 오운완!",
+  //       comments: []
+  //     }
+  //   ]
+  // }
+
+  function getArticleLists() {
+    axios.get("http://localhost:8080/post/ohwunwan")
+    .then(response => {
+      console.log(response);
+      setArticleLists(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
+
+  useEffect(() => {
+    if(articleLists.length === 0) {
+      getArticleLists()
+    }
+  },[articleLists])
 
   return (
     <MainWrap>
-      {/* {articleLists.articles.map((obj, idx) => 
-        <Fragment key={idx}>
-          <Article />
-        </Fragment>
-      )} */}
-
-      <Article {...articleLists} />
+      {/* <Article {...articleLists} /> */}
+      {articleLists.map((obj, idx) => {
+        return (
+          <p key={idx}>{obj.userId}</p>
+        )
+      })}
     </MainWrap>
   )
 }
