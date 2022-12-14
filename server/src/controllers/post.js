58a3722a0f303c08f0ae5fcd10f6f12b39b7a544
hyae4924 <hyae4924@gmail.com>
2022-12-15 04:15:25 +0900

    [FIX] Fix post controller code convention

--- server/src/controllers/post.js
+++ server/src/controllers/post.js
@@ -1,33 +1,37 @@
 import * as postServices from "../services/post.js";
 export const getPost = async (req, res) => {
   const { params, query } = req;
-  const data = await postServices.getPost(params, query);
-  if (data.length == 0) return res.status(204).json();
-  if (data) return res.status(200).json(data);
-  else return res.status(500).json({ message: "Something is wrong" });
+  return await postServices
+    .getPost(params, query) //
+    .then(data => {
+      if (data.length == 0) return res.status(204).json("no content");
+      else if (data) return res.status(200).json(data);
+    });
 };
 
 export const createPost = async (req, res) => {
   const { params, body, files } = req;
-  const data = await postServices.createPost(params, body, files);
-  if (data) return res.status(201).json(data);
-  else return res.status(500).json({ message: "Something is wrong" });
+  return await postServices
+    .createPost(params, body, files) //
+    .then(data => {
+      return res.status(201).json(data);
+    });
 };
 export const updatePost = async (req, res) => {
   const { params, body } = req;
-  const data = await postServices.updatePost(params, body);
-  if (data.length == 0) return res.status(204).json();
-  if (data) return res.status(201).json(data);
-  else return res.status(500).json({ message: "Something is wrong" });
+  return await postServices
+    .updatePost(params, body) //
+    .then(data => {
+      if (data.length == 0) return res.status(204).json("no content");
+      else if (data) return res.status(200).json(data);
+    });
 };
+
 export const removePost = async (req, res) => {
   const { params, query } = req;
-  postServices
+  return postServices
     .removePost(params, query) //
     .then(result => {
       return res.status(200).json({ message: "ok" });
-    })
-    .catch(err => {
-      if (err) return res.status(500).json({ message: "Something is wrong" });
     });
 };
