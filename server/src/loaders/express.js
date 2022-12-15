01702573e014942b91ecc86dea3c4efd7f909000
hyae4924 <hyae4924@gmail.com>
2022-12-16 01:07:42 +0900

    [FEAT] Add cors option

--- server/src/loaders/express.js
+++ server/src/loaders/express.js
@@ -1,30 +1,37 @@
-import {} from "express-async-errors";
-import { ValidationError } from "../errors/validationError.js";
-import { MulterError } from "multer";
 import express from "express";
+import {} from "express-async-errors";
+import { MulterError } from "multer";
 import cors from "cors";
 import moran from "morgan";
 import helmet from "helmet";
 import config from "../config/config.js";
 import postRouter from "../routes/post.js";
 import authRouter from "../routes/auth.js";
+import { ValidationError } from "../errors/validationError.js";
 
 export default async ({ app }) => {
-  app.use(cors()); //cors set
+  // ----기본세팅----
+  const corsOption = {
+    origin: config.cors.allowedOrigin,
+    optionsSuccessStatus: 200,
+    credentials: true, // allow the Access-Control-Allow-Credentials 추가해줌
+  };
+  app.use(cors(corsOption)); //cors set
   app.use(helmet());
   app.use(express.json()); // REST API의 body를 조회가능하게해줌 (내부미들웨어)
   if (config.env === "development") app.use(moran("dev")); // log set
   else app.use(moran("combined"));
 
+  // ----라우팅----
   app.use("/post", postRouter);
   app.use("/auth", authRouter);
 
-  //잘못 된 경로입력시
+  //----잘못 된 경로입력시----
   app.use((req, res, next) => {
     res.status(404).end();
   });
 
-  //비동기 에러도 잡을 수 있음 ("express-async-errors";)
+  //----에러케치 (비동기 에러도 잡을 수 있음 )("express-async-errors";)----
   app.use((err, req, res, next) => {
     if (err instanceof ValidationError) {
       return res.status(400).json({ message: err });
